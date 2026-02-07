e50f47bc5554537f8362ddc2ce188d0d61e5c3d1
David Carrasco <davidack123456789@gmail.com>
2026-02-06 23:12:32 -0500

    Se enlaza y se añaden algunos pasos para seguir en nuestra codigo para la interactividad de la pagina

---
+++ Landing-page-Atalaya-Studio/main.js
@@ -0,0 +1,26 @@
+//1. Helpers (pequeñas funciones para reutilizar)
+
+
+//2. Formulario
+function initForm(){
+
+}
+
+//3. Menu mobile
+
+function initMenu(){
+
+}
+
+//4. Testimonios API
+
+async function loadTestimonials() {
+    
+}
+
+//5. Inicializacion
+document.addEventListener("DOMContentLoaded", () => {
+    initForm()
+    initMenu()
+    loadTestimonials()
+})
